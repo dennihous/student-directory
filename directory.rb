08297bb193f4c1e4011bf845da265a02b7c9d4e4
dennis <denhouston1@icloud.com>
2022-02-15 11:47:16 +0000

    updated final

--- directory.rb
+++ directory.rb
@@ -1,34 +1,3 @@
-#array of students
-
-# students = [
-#     {name: "Dr. Hannibal Lecter", cohort: :november},
-#     {name: "Darth Vader", cohort: :november},
-#     {name: "Nurse Ratched", cohort: :november},
-#     {name: "Michael Corleone", cohort: :november},
-#     {name: "Alex DeLarge", cohort: :november},
-#     {name: "The Wicked Witch of the West", cohort: :november},
-#     {name: "Terminator", cohort: :november},
-#     {name: "Freddy Krueger", cohort: :november},
-#     {name: "The Joker", cohort: :november},
-#     {name: "Joffrey Baratheon", cohort: :november},
-#     {name: "Norman Bates", cohort: :november}
-# ]
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "----------"
-end
-
-def print(names)
-  names.each do |name|
-    puts "#{name[:name]} (#{name[:cohort]} cohort)"
-  end
-end
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
@@ -46,6 +15,21 @@ def input_students
   end
   # return the array of students
   students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------"
+end
+
+def print(names)
+  names.each do |name|
+    puts "#{name[:name]} (#{name[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
 students = input_students
