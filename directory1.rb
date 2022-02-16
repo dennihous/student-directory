4b70dc61353c1570cffcb3930c1aa64d19685050
dennis <denhouston1@icloud.com>
2022-02-16 18:11:31 +0000

    adding spelling mistake and cohort options

--- directory1.rb
+++ directory1.rb
@@ -1,17 +1,46 @@
 def input_students
-  puts "Please enter the name of the students"
-  puts "To finish, just hit return twice"
+  puts "Enter the name of the student"
+  puts "To finish, enter empty results for cohort and name"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  # while the name is nt empty, repeat this code
-  while !name.empty? do
+  name = "Unknown" if name.empty?
+  puts "Please enter the name of the cohort"
+  # while the name is not empty, repeat this code
+
+  months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+  while true
+    cohort = gets.chomp.downcase
+    if cohort.empty?
+      cohort = "Unknown"
+      break
+    elsif months.include?(cohort)
+      cohort
+      break
+    end
+  end
+  
+  until name == "Unknown" && cohort == "Unknown" do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{students.count/2} students"
     # get another name from the user
+    puts "Enter the name of the student"
     name = gets.chomp
+    name = "Unknown" if name.empty?
+    puts "Enter the name of the cohort"
+    while true
+      cohort = gets.chomp.downcase
+      if cohort.empty?
+        cohort = "Unknown"
+        break
+      elsif months.include?(cohort)
+        cohort
+        break
+      end
+    end
   end
   # return the array of students
   students
@@ -24,15 +53,15 @@ end
 
 def print(names)
   names.each do |name|
-    puts "#{name[:name]} (#{name[:cohort]} cohort)".center(50)
+    puts "#{name[:name]} (#{name[:cohort].capitalize} cohort)"
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count/2} great students"
 end
 
 students = input_students
 print_header
 print(students)
-print_footer(students)
+print_footer(names)
